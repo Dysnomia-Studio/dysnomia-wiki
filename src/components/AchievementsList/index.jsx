ce20ba76e298883de660d02deff2194a93bd8463
Elanis <elanis@hotmail.com>
2024-05-10 13:27:12 +0200

    Refacto achievements list

---
+++ src/components/AchievementsList/index.jsx
@@ -0,0 +1,27 @@
+import './index.css';
+
+export default function AchievementsList({ achievements, t }) {
+	console.log(achievements);
+	return (
+		<div class="achievement-list">
+			{achievements.map((currentAchievement) => {
+				return (
+					<div class="achievement-list-item">
+						<img src={`/img/alchemistry/achievements/${currentAchievement.name}_ok.png`} />
+						<div class="achievement-list-item-body">
+							<div class="achievement-list-item-title">
+								{t(`alchemistry:achievements:NEW_ACHIEVEMENT_1_${currentAchievement.index}_NAME`)}
+							</div>
+							<span class={'achievement-list-rarity' + ((currentAchievement.percent < 10) ? ' achievement-list-rarity-rare' : '')}>
+								{Math.floor(currentAchievement.percent)}%
+							</span>
+							<div class="achievement-list-item-description">
+								{currentAchievement.description}
+							</div>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
---
+++ src/components/AlchemistryAchievementDescription/index.jsx
@@ -0,0 +1,13 @@
+import { neededItems, neededIngredients } from '/src/business/alchemistry_neededItems.js';
+
+export default function AlchemistryAchievementDescription({ achievement, t }) {
+	return (
+		<div>
+			{t(`alchemistry:achievements:NEW_ACHIEVEMENT_1_${achievement.index}_DESC`).replace('N/A', '')}
+			{neededItems[achievement.name] && <details>
+				<summary>Item needed</summary>{/* TODO: i18n */}
+				You should create <b>{neededItems[achievement.name]}</b>{neededIngredients[achievement.name] && ` using ${neededIngredients[achievement.name]}`}.
+			</details>}
+		</div>
+	);
+}
