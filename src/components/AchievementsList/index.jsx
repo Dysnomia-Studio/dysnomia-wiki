d701d13a27bec1d81c241abb4b9fdbf015631b9b
Elanis <elanis@hotmail.com>
2024-05-10 14:04:40 +0200

    Improve achievements accessibility

--- src/components/AchievementsList/index.jsx
+++ src/components/AchievementsList/index.jsx
@@ -13,7 +13,7 @@ export default function AchievementsList({ achievements, t }) {
 
 				return (
 					<div key={currentAchievement.index} className="achievement-list-item">
-						<img src={`/img/alchemistry/achievements/${currentAchievement.name}_ok.png`} />
+						<img src={`/img/alchemistry/achievements/${currentAchievement.name}_ok.png`} width="50" height="50" alt={t(`alchemistry:achievements:NEW_ACHIEVEMENT_1_${currentAchievement.index}_NAME`)} />
 						<div className="achievement-list-item-body">
 							<div className="achievement-list-item-title">
 								{t(`alchemistry:achievements:NEW_ACHIEVEMENT_1_${currentAchievement.index}_NAME`)}
