338c0f9af02f8fc0faa951fd0a67b05f03d99d5b
Elanis <elanis@hotmail.com>
2024-05-10 14:47:11 +0200

    Add footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,7 @@
+import './index.css';
+
 export default function Footer() {
 	return (
-		<span>Test</span>
+		<footer>Alchemistry, Extortion, Manufactur'inc - Made by Dysnomia - <a href="https://dysnomia.studio" target="_blank" rel="noopener noreferrer">https://dysnomia.studio</a></footer>
 	);
 }
