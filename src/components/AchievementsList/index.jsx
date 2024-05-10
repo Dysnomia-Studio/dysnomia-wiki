19af60aedea9c2f9d799c9bd3fa2634be6a19d38
Elanis <elanis@hotmail.com>
2024-05-10 13:50:26 +0200

    Improve code quality

--- src/components/AchievementsList/index.jsx
+++ src/components/AchievementsList/index.jsx
@@ -1,20 +1,27 @@
 import './index.css';
 
+const RARITY_THRESOLD = 10;
+
 export default function AchievementsList({ achievements, t }) {
 	return (
-		<div class="achievement-list">
+		<div className="achievement-list">
 			{achievements.map((currentAchievement) => {
+				let rarityClass = 'achievement-list-rarity';
+				if(currentAchievement.percent < RARITY_THRESOLD) {
+					rarityClass += ' achievement-list-rarity-rare';
+				}
+
 				return (
-					<div class="achievement-list-item">
+					<div key={currentAchievement.index} className="achievement-list-item">
 						<img src={`/img/alchemistry/achievements/${currentAchievement.name}_ok.png`} />
-						<div class="achievement-list-item-body">
-							<div class="achievement-list-item-title">
+						<div className="achievement-list-item-body">
+							<div className="achievement-list-item-title">
 								{t(`alchemistry:achievements:NEW_ACHIEVEMENT_1_${currentAchievement.index}_NAME`)}
 							</div>
-							<span class={'achievement-list-rarity' + ((currentAchievement.percent < 10) ? ' achievement-list-rarity-rare' : '')}>
+							<span className={rarityClass}>
 								{Math.floor(currentAchievement.percent)}%
 							</span>
-							<div class="achievement-list-item-description">
+							<div className="achievement-list-item-description">
 								{currentAchievement.description}
 							</div>
 						</div>
