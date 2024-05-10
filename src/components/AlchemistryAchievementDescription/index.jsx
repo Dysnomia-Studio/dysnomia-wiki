6de8c76ec0a04e5d08a5649f5dad660149651180
Elanis <elanis@hotmail.com>
2024-05-10 20:52:36 +0200

    Alchemistry achievements: Items links + translation

--- src/components/AlchemistryAchievementDescription/index.jsx
+++ src/components/AlchemistryAchievementDescription/index.jsx
@@ -1,12 +1,17 @@
 import { neededItems, neededIngredients } from '/src/business/alchemistry_neededItems.js';
 
-export default function AlchemistryAchievementDescription({ achievement, t }) {
+export default function AlchemistryAchievementDescription({ achievement, t, translatePath }) {
 	return (
 		<div>
 			{t(`alchemistry:achievements:NEW_ACHIEVEMENT_1_${achievement.index}_DESC`).replace('N/A', '')}
 			{neededItems[achievement.name] && <details>
 				<summary>Item needed</summary>{/* TODO: i18n */}
-				You should create <b>{neededItems[achievement.name]}</b>{neededIngredients[achievement.name] && <> using <b>{neededIngredients[achievement.name]}</b>.</>}
+				You should create <a href={translatePath(`/items/${neededItems[achievement.name]}`)}>
+					{t(`alchemistry:items:${neededItems[achievement.name]}`)}
+				</a>{neededIngredients[achievement.name] && <> using <a href={translatePath(`/items/${neededIngredients[achievement.name]}`)}>
+					{t(`alchemistry:items:${neededIngredients[achievement.name]}`)}
+					</a>.
+				</>}
 			</details>}
 		</div>
 	);
