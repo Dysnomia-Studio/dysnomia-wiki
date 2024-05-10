b3c8003644dae4f2d1c7f11edd7856af108e29b2
Elanis <elanis@hotmail.com>
2024-05-10 14:23:48 +0200

    Auto-generate robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import playformCompress from "@playform/compress";
-
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), playformCompress(), 
-  	sitemap({
-      i18n: {
-        defaultLocale: 'en',
-        locales: {
-          en: 'en-US',
-          fr: 'fr-FR',
-        },
-      },
-  	})],
+  integrations: [react(), playformCompress(), sitemap({
+    i18n: {
+      defaultLocale: 'en',
+      locales: {
+        en: 'en-US',
+        fr: 'fr-FR'
+      }
+    }
+  }), robotsTxt()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
@@ -23,5 +23,5 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  site: 'https://wiki.dysnomia.studio',
+  site: 'https://wiki.dysnomia.studio'
 });
