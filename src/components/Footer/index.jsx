0d58798bff1176416408d7bc75b6a90b175f012f
Elanis <elanis@hotmail.com>
2024-05-18 12:38:33 +0200

    Footer: add github link and add Galactae

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,6 +2,6 @@ import './index.css';
 
 export default function Footer() {
 	return (
-		<footer>Alchemistry, Extortion, Manufactur'inc - Made by Dysnomia - <a href="https://dysnomia.studio" target="_blank" rel="noopener noreferrer">https://dysnomia.studio</a></footer>
+		<footer>Alchemistry, Extortion, Galactae, Manufactur'inc - Made by Dysnomia - <a href="https://dysnomia.studio" target="_blank" rel="noopener noreferrer">https://dysnomia.studio</a> - <a href="https://github.com/Dysnomia-Studio/dysnomia-wiki" target="_blank" rel="noopener noreferrer">Github sources</a></footer>
 	);
 }
