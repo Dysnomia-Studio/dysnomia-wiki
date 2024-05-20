3a5a634db62d7083121226ad348024ae9ee8087d
Elanis <elanis@hotmail.com>
2024-05-20 14:58:09 +0200

    Add language picker to footer

--- src/components/Footer/index.jsx
+++
@@ -1,7 +0,0 @@
-import './index.css';
-
-export default function Footer() {
-	return (
-		<footer>Alchemistry, Extortion, Galactae, Manufactur'inc - Made by Dysnomia - <a href="https://dysnomia.studio" target="_blank" rel="noopener noreferrer">https://dysnomia.studio</a> - <a href="https://github.com/Dysnomia-Studio/dysnomia-wiki" target="_blank" rel="noopener noreferrer">Github sources</a></footer>
-	);
-}
