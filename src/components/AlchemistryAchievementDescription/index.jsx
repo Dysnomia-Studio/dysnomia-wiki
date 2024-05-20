55f9d995f3c4967cf9a0606356240b7e3ffd9654
Elanis <elanis@hotmail.com>
2024-05-20 15:22:07 +0200

    Add alchemistry's achievements translations

--- src/components/AlchemistryAchievementDescription/index.jsx
+++ src/components/AlchemistryAchievementDescription/index.jsx
@@ -8,10 +8,10 @@ export default function AlchemistryAchievementDescription({ achievement, t, tran
 		<div>
 			{t(`alchemistry:achievements:NEW_ACHIEVEMENT_1_${achievement.index}_DESC`).replace('N/A', '')}
 			{neededItems[achievement.name] && <details>
-				<summary>Item needed</summary>{/* TODO: i18n */}
-				You should create <a href={translatePath(`/items/${neededItems[achievement.name]}`)}>
+				<summary>{t(`alchemistry:wiki:achievement-item-needed`)}</summary>
+				{t(`alchemistry:wiki:achievement-item-description-need`)} <a href={translatePath(`/items/${neededItems[achievement.name]}`)}>
 					{t(`alchemistry:items:${neededItems[achievement.name]}`)}
-				</a>{neededIngredients[achievement.name] && <> from <a href={translatePath(`/items/${neededIngredients[achievement.name]}`)}>
+				</a>{neededIngredients[achievement.name] && <> {t(`alchemistry:wiki:achievement-item-description-from`)} <a href={translatePath(`/items/${neededIngredients[achievement.name]}`)}>
 					{t(`alchemistry:items:${neededIngredients[achievement.name]}`)}
 					</a>.
 				</>}
