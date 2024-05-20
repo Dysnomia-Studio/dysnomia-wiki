6db59b007e4b760cef158ab510445db9f702f8ce
Elanis <elanis@hotmail.com>
2024-05-20 16:05:17 +0200

    Mobile menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -2,19 +2,23 @@ import './index.css';
 
 export default function Menu() {
 	return (
-		<nav className="menu">
-			<a href="/"><img src="/dysnomia.webp" alt="Dysnomia's logo" /></a>
-			<span className="menu-item">
-				<span className="menu-item-title">Alchemistry</span>
-				<span className="submenu">
-					<a href="/alchemistry/achievements">Achievements</a><br/>
-					<a href="/alchemistry/challenges">Challenges</a><br/>
-					<a href="/alchemistry/items">Items</a><br/>
+		<>
+			<a href="#menu" className="mobile-menu-button">☰</a>
+			<nav id="menu" className="menu">
+				<a href="#" className="mobile-menu-quit">X</a>
+				<a href="/"><img src="/dysnomia.webp" alt="Dysnomia's logo" /></a>
+				<span className="menu-item">
+					<span className="menu-item-title">Alchemistry</span>
+					<span className="submenu">
+						<a href="/alchemistry/achievements">Achievements</a><br/>
+						<a href="/alchemistry/challenges">Challenges</a><br/>
+						<a href="/alchemistry/items">Items</a><br/>
+					</span>
 				</span>
-			</span>
-			<a href="/extortion">Extortion</a>
-			<a href="/galactae">Galactae</a>
-			<a href="/manufactur-inc">Manufactur'inc</a>
-		</nav>
+				<a className="menu-item" href="/extortion">Extortion</a>
+				<a className="menu-item" href="/galactae">Galactae</a>
+				<a className="menu-item" href="/manufactur-inc">Manufactur'inc</a>
+			</nav>
+		</>
 	);
 }
