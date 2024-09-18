27a49fd9620df2de99115f303ec9cf2a9c6c0f3c
Elanis <elanis@hotmail.com>
2024-09-18 23:26:15 +0200

    Fix arabic pages build

--- copy-pages-for-other-languages.sh
+++ copy-pages-for-other-languages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for lang in {"de","fr","nl","ru","sv"}; do 
+for lang in {"ar","de","fr","nl","ru","sv"}; do 
 	cp -R src/pages/alchemistry/ src/pages/$lang/
 	cp -R src/pages/manufactur-inc/ src/pages/$lang/
 	cp -R src/pages/*.astro src/pages/$lang/
